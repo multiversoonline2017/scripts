16147a7afa0ec4701bafe75d25181b64c29224c6
XeZZ <distruct3r@web.de>
2018-11-10 16:19:06 +0100

    update 2.0

---
+++ APR/update_200.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+echo "ONLY USE THIS SCRIPT IF YOU USED THE NORMAL 'setup.sh' script - IF YOU USED THE 'setup_multi.sh' USE THE APPRECIATE UPDATE SCRIPT"
+echo "BEFORE YOU PROCEED MAKE SURE YOUR GUI WALLET (WIN/MAC) IS UPDATED ALREADY!"
+echo "Enter 1 to update, enter 2 to check status (Press enter after) ONLY UPDATE ONCE!!!!!"
+read UPDATE
+
+if [[ $UPDATE =~ "1" ]] ; then
+    echo "You have to go to your GUI Wallet now and restart ALL nodes on it! before you continue! Type yes to proceed"
+    read OK
+    if [[ $OK =~ "yes" ]]; then
+
+      aprcoin-cli stop
+
+      ## COMPILE AND INSTALL
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-cli
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-qt
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-tx
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoind
+      sudo chmod 755 aprcoin*
+      sudo mv aprcoin* /usr/bin
+
+      rm .apr*/mn*
+
+      echo "Wait for shutdowns..."
+      sleep 60
+
+      aprcoind
+
+      echo "Wait for blockchain to restart... (ca 3 mins)"
+
+      sleep 180
+
+      aprcoin-cli masternode status
+      echo "Your nodes have been updated and restarted. Please wait 5 minutes and check the status if not active already!"
+    else
+      echo "SCRIPT WAS BROKEN BECAUSE YOU DID NOT READ OR TYPED yes ! Start again and follow instructions"
+    fi
+else
+    aprcoin-cli masternode status
+fi
---
+++ APR/update_multi_200.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+echo "ONLY USE THIS SCRIPT IF YOU USED THE MULTI 'setup_multi.sh' script - IF YOU USED THE NORMAL 'setup.sh' USE THE APPRECIATE UPDATE SCRIPT"
+echo "BEFORE YOU PROCEED MAKE SURE YOUR GUI WALLET (WIN/MAC) IS UPDATED ALREADY!"
+echo "Enter 1 to update, enter 2 to check status (Press enter after) ONLY UPDATE ONCE!!!!!"
+read UPDATE
+
+if [[ $UPDATE =~ "1" ]] ; then
+    echo "You have to go to your GUI Wallet now and restart ALL nodes on it! before you continue! Type yes to proceed"
+    read OK
+    if [[ $OK =~ "yes" ]]; then
+
+      for filename in bin/aprcoin-cli_*.sh; do
+        sh $filename stop
+      done
+
+      ## COMPILE AND INSTALL
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-cli
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-qt
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoin-tx
+      wget https://github.com/XeZZoR/scripts/raw/master/APR/aprcoind
+      sudo chmod 755 aprcoin*
+      sudo mv aprcoin* /usr/bin
+
+      rm .apr*/mn*
+
+      echo "Wait for shutdowns..."
+      sleep 60
+
+      for filename in bin/aprcoind_*.sh; do
+        sh $filename
+      done
+      echo "Wait for blockchain to restart... (ca 3 mins)"
+
+      sleep 180
+
+      for filename in bin/aprcoin-cli_*.sh; do
+        sh $filename masternode status
+      done
+      echo "Your nodes have been updated and restarted. Please wait 5 minutes and check the status if not active already!"
+    else
+      echo "SCRIPT WAS BROKEN BECAUSE YOU DID NOT READ OR TYPED yes ! Start again and follow instructions"
+    fi
+else
+  for filename in bin/aprcoin-cli_*.sh; do
+    sh $filename masternode status
+  done
+fi
