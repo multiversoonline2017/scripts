1160dea0e98dca932a412c703ec4dcd060e5e93b
XeZZ <distruct3r@web.de>
2018-12-19 16:11:35 +0100

    New nodes - remove update scripts

--- APR/setup_multi.sh
+++ APR/setup_multi.sh
@@ -126,31 +126,18 @@ for i in `seq 1 1 $MNCOUNT`; do
   echo "masternodeaddr=$IP:$PORT" >> ${NAME}.conf_TEMP
   echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
 
-    echo "addnode=45.77.227.93" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.191.175.138" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.188.93.12" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=85.214.199.57" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.222.190.189" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=3.16.148.30" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=209.97.183.127" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=3.16.166.214" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=108.61.175.177" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=142.93.32.206" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=178.128.166.102" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=140.82.29.242" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=104.156.232.221" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=167.99.160.210" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=52.14.225.17" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=3.16.29.195" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.220.105.129" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.224.183.32" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.218.246.251" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=95.179.139.68" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=3.16.10.163" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=18.188.164.171" >> $CONF_DIR/$CONF_FILE
-    echo "addnode=3.16.125.132" >> $CONF_DIR/$CONF_FILE
-
-
+  echo "addnode=addnode=142.93.142.33" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=206.189.160.179" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=178.128.166.102" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=167.99.171.144" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=209.97.183.237" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=104.248.244.144" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=159.89.207.84" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=80.85.158.36" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=91.211.248.160" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=164.132.135.102" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=185.235.131.132" >> $CONF_DIR/$CONF_FILE
+  echo "addnode=addnode=178.159.38.187" >> $CONF_DIR/$CONF_FILE
   sudo ufw allow $PORT/tcp
 
   mv ${NAME}.conf_TEMP $CONF_DIR/${NAME}.conf
